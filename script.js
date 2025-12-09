8604c06a474a485cc705f1109b880caa3bffdf03
ElvisBinaku <elvisbinaku@icloud.com>
2025-12-09 20:11:28 +0100

    sistemato l'else er il fizzbuzz mettendolo come primo if

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-let number;
-let multipli3;
-let multipli5;
-let multipli3e5;
-
 for (let number = 1; number <= 100; number++){ 
-
-    if (number % 3 === 0) {
-        console.log("Fizz");
+// controllo se il numero è divisibile per 3  e per 5
+    if (number % 3 === 0 && number % 5 === 0) {
+        console.log("FizzBuzz");
+        // controllo se il numero è divisibile per 5
     } else if (number % 5 === 0) { 
-        console.log("Buzz")
-    } else if (number % 3 === 0 && number % 5 === 0) {
-        console.log("FizzBuzz")
+        console.log("Buzz");
+        //controllo se il numero è divisibile per 3
+    } else if (number % 3 === 0) {
+        console.log("Fizz");
     } else {
-        console.log(number)
+        console.log(number);
     }
 }
 
